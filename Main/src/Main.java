1d97f814717aa56d408bbe5aa157d5d0dd0aeb42
meowseal <hotdogtoiletcleaner@gmail.com>
2025-12-11 11:58:30 -0600

    Moved Circle to Object files & basic main module for now

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -5,7 +5,7 @@ public class Main {
     static Circle shape = new Circle(3,5,0,0);
     public static void main(String[] args)
         {
-            System.out.println();
+            System.out.println(shape.getWeight());
         }
 
 
